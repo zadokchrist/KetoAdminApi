fba9ad4c767a881c1fc9501a24a7df78fc2284d1
NGOBI DANIEL <ngobizadokchrist@gmail.com>
2024-07-11 14:36:18 +0300

    addition of patient model

--- Domain/Models/Patient.cs
+++ Domain/Models/Patient.cs
@@ -8,5 +8,16 @@ namespace Domain.Models
 {
     public class Patient
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Mobile {  get; set; }
+        public DateOnly DateOfBirth {  get; set; }
+        public string Gender {  get; set; }
+        public string WhatsappNumber {  get; set; }
+        public string Email {  get; set; }
+        public string Address {  get; set; }
+
+
     }
 }
--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0faef4196178ecab040b3bae55b27a217766390c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
